3f4b48218b8a8ae5631373329be95d32640fe418
Abdul Rafay Khan <rafaykhan.work@gmail.com>
2022-03-08 14:39:05 +0300

    worked

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -12,7 +12,7 @@ function Homepage() {
 
     const { Title } = Typography;
 
-    const { data, isFetching } = useGetCryptosQuery();
+    const { data, isFetching } = useGetCryptosQuery(10);
     const globalStats = data?.data?.stats;
 
     if (isFetching) return 'Loading...';
@@ -44,12 +44,12 @@ function Homepage() {
                 <Title level={2} className="home-title">Top 10 Currencies in the world</Title>
                 <Title level={3} className="show-more"><Link to="/cryptocurrencies">Show More</Link></Title>
             </div>
-            <Cryptocurrencies simplified/>
+            <Cryptocurrencies simplified />
             <div className="home-heading-container">
                 <Title level={2} className="home-title">Latest Crypto News</Title>
                 <Title level={3} className="show-more"><Link to="/news">Show More</Link></Title>
             </div>
-            <News simplified/>
+            <News simplified />
         </>
     )
 }
